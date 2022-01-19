36f0f607fda02cab32f1baf2c73521c7b294386a
askhyuk <97231660+askhyuk@users.noreply.github.com>
2022-01-19 22:00:32 +0900

    엑셀 호환,음수일 경우 1 더해줌

--- fixed_mult.v
+++ fixed_mult.v
@@ -7,6 +7,8 @@ function [15:0] fixed_mult(
         b_32 = b;
         c_32 = a_32*b_32;
         c_32 = (c_32 >> 8);
+        if(c_32[15] == 1'b1) c_32 = c_32 + 1'b1;
+        else c_32 = c_32;
         fixed_mult = c_32[15:0];
     end
 endfunction
